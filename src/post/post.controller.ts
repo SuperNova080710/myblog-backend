9c6a6d03e05b6979b0965be567c0228f16c9c3e6
SuperNova080710 <lionoh00@gmail.com>
2026-01-18 20:30:44 +0900

    add function update, delete / add dto

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Get, Post as HttpPost, Body } from '@nestjs/common';
+import { Controller, Get, Post as HttpPost, Body, Patch, Param, Delete } from '@nestjs/common';
 import { PostService } from './post.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @HttpPost()
-  create(@Body() body: { title: string; detail: string }) {
-    return this.postService.create(body.title, body.detail);
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postService.create(createPostDto);
+  }
+
+  @Patch('id')
+  update(
+    @Param('id') id: string,
+    @Body() updatePostDto: UpdatePostDto,
+  ) {
+    return this.postService.update(Number(id), updatePostDto);
+  }
+
+  @Delete('id')
+  remove(@Param('id') id: string) {
+    return this.postService.remove(Number(id));
   }
 
   @Get()
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Post } from './post.entity';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostService {
@@ -10,9 +12,33 @@ export class PostService {
     private readonly postRepository: Repository<Post>,
   ) {}
 
-  create(title: string, detail: string) {
-    const post = this.postRepository.create({ title, detail });
+  create(createPostDto: CreatePostDto) {
+    const post = this.postRepository.create(createPostDto);
     return this.postRepository.save(post);
+  }
+
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    const post = await this.postRepository.findOne({
+      where: {id},
+    });
+
+    if(!post) {
+      throw new NotFoundException('Post not found');
+    }
+
+    Object.assign(post, updatePostDto);
+
+    return this.postRepository.save(post);
+  }
+
+  async remove(id: number) {
+    const result = await this.postRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException('Post not found');
+    }
+
+    return {success: true};
   }
 
   findAll() {
